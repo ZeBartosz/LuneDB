bae000cd7a3793631afe5cd8a0989be10661d75c
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-09-20 16:38:25 +0200

    refactor: update the fuction style

--- src/ast/expression.cs
+++ src/ast/expression.cs
@@ -1,28 +1,17 @@
 namespace LuneDB
 {
-
     // Literal Expressions
     public struct NumberExpr : IExpr
     {
         public double value;
-
-        public NumberExpr(double value)
-        {
-            this.value = value;
-        }
-
+        public NumberExpr(double value) => this.value = value;
         public void expr() { }
     }
 
-    public struct StringExpr : IExpr
+    public struct IdentifierExpr : IExpr
     {
         public string value;
-
-        public StringExpr(string value)
-        {
-            this.value = value;
-        }
-
+        public IdentifierExpr(string value) => this.value = value;
         public void expr() { }
     }
 
@@ -32,14 +21,12 @@ namespace LuneDB
         public IExpr left;
         public Token.TokenType token;
         public IExpr right;
-
         public BinaryExpr(IExpr left, Token.TokenType token, IExpr right)
         {
             this.left = left;
             this.token = token;
             this.right = right;
         }
-
         public void expr() { }
     }
 }
