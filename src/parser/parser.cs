b9f986ddbe4f2ba5184f0092edd0072a9e22161f
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-09-20 18:07:39 +0200

    refactor: move Tokenize to the top

--- src/parser/parser.cs
+++ src/parser/parser.cs
@@ -3,71 +3,53 @@ namespace LuneDB
     public class Parser
     {
         private IReadOnlyList<Token> _tokens { get; }
-        private int pos = 0;
+        private int _pos = 0;
 
-        public Parser(IReadOnlyList<Token> tokens)
-        {
-            _tokens = tokens;
-            this.pos = 0;
-        }
+        public Parser(IReadOnlyList<Token> tokens) => _tokens = tokens;
 
         // Helpers
-        public Token currentToken()
-        {
-            if (pos >= _tokens.Count)
-            {
-                return new Token(Token.TokenType.EOF, "eof");
-            }
-            return _tokens[pos];
+        public bool HasTokens() => _pos < _tokens.Count && CurrentTokenType() != Token.TokenType.EOF;
+        public Token.TokenType CurrentTokenType() => CurrentToken().Type;
 
+        public Token CurrentToken()
+        {
+            if (_pos >= _tokens.Count) return new Token(Token.TokenType.EOF, "eof");
+
+            return _tokens[_pos];
         }
 
-        public Token.TokenType currentTokenType()
+        public Token Advance()
         {
-            return currentToken().Type;
+            if (_pos < _tokens.Count) _pos++;
+
+            return CurrentToken();
         }
 
-        public Token advance()
+
+        public Token ExpectError(Token.TokenType expectedToken, string errorMsg)
         {
-            if (pos < _tokens.Count) { pos++; }
-
-
-            return currentToken();
-        }
-
-        public bool hasTokens()
-        {
-            return pos < _tokens.Count && currentTokenType() != Token.TokenType.EOF;
-        }
-
-        public Token expectError(Token.TokenType expectedToken, string errorMsg)
-        {
-            Token currentToken = this.currentToken();
+            Token currentToken = CurrentToken();
 
             if (currentToken.Type != expectedToken)
-            {
                 throw new Exception($"expected \"{expectedToken}\", but received \"{currentToken.Type}\"");
-            }
 
-            return advance();
+            return Advance();
         }
 
         public IStmt Parse()
         {
             GlobalLookup.createLookup();
+
             Parser parser = new Parser(_tokens);
-            StmtParser stmtParser = new StmtParser(parser);
             List<IStmt> body = new List<IStmt>();
 
-            while (parser.hasTokens())
+            while (parser.HasTokens())
             {
-                IStmt stmt = stmtParser.ParseStmt();
+                IStmt stmt = StmtParser.ParseStmt(parser);
                 body.Add(stmt);
             }
 
             return new BlockStmt(body);
         }
-
     }
-
 }
