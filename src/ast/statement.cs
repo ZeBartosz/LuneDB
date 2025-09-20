602e6df9acdafe8c97352deabd8289724acb0372
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-09-20 16:38:48 +0200

    feat: add expression statemnt

--- src/ast/statement.cs
+++ src/ast/statement.cs
@@ -1,14 +1,16 @@
 namespace LuneDB
 {
+    public struct ExpressionStmt : IStmt
+    {
+        public IExpr expression;
+        public ExpressionStmt(IExpr expression) => this.expression = expression;
+        public void stmt() { }
+    }
+
     public struct BlockStmt : IStmt
     {
         public List<IStmt> body;
-
-        public BlockStmt(List<IStmt> body)
-        {
-            this.body = body;
-        }
-
+        public BlockStmt(List<IStmt> body) => this.body = body;
         public void stmt() { }
     }
 }
